14302e9ca71272c363f085de60d4fbfa57bc1aea
Loren M. Kerr <loren@lorenkerr.com>
2020-12-09 20:09:37 -0700

    updated header

--- app/src/components/header/index.js
+++ app/src/components/header/index.js
@@ -5,7 +5,7 @@ import style from './style.css';
 const Header = () => (
 	<header class={style.header}>
 			<img src="/assets/img/unicorn-logo.svg" alt="ToggleMiser Unicorn Logo"/>
-			<span class="header-title">TDSC ToggleMiser</span>
+			<span class="header-title">ToggleMiser</span>
 		{/* <nav>
 			<Link activeClassName={style.active} href="/">Home</Link>
 			<Link activeClassName={style.active} href="/billing">Billing</Link>
